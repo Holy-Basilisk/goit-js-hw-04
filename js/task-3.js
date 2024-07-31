e03e6d4681fc4fd1ed053fad9f167eeca711f9f7
Paul <mailpiligrim@gmail.com>
2024-07-31 12:16:43 +0300

    small fix

--- js/task-3.js
+++ js/task-3.js
@@ -7,7 +7,7 @@ const profile = {
         this.username = newName;
     },
     updatePlayTime(hours) {
-        this.playTime = hours;
+        this.playTime += hours;
     },
     getInfo() {
         return `${this.username} has ${this.playTime} active hours!`
@@ -22,4 +22,4 @@ profile.changeUsername("Marco");
 console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 20 active hours!"
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
